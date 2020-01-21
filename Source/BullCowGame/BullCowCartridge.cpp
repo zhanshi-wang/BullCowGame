fb01bab001e2559213a6596fbc4d4be298831c2b
Zhanshi Wang <zhanshi.wang.verdandi@gmail.com>
2020-01-20 18:43:05 -0800

    bullcow in progress backup

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,14 +4,57 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord);
+
+    // Welcoming the player
     PrintLine(TEXT("Welcome to the Bull Cows!"));
-    PrintLine(TEXT("Press Tab to enter the TERMINAL..."));
-    PrintLine(TEXT("Press Enter to continue..."));
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and press Enter to continue..."));
+
+    // Prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
 
-    FString HiddenWord = TEXT("subdermatoglyphic");
+    // Checking player guess
+    // Check if isogram
+    // Guess Again
+    // Check right number of characters
+    // Guess Again
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win!"));
+        // Play Again?
+    }
+    else
+    {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is %i characters long, try again!"), HiddenWord.Len());
+        }
+        PrintLine(TEXT("You lose!"));
+        // Remove one life
+        // If Life <= 0
+        // Show game over
+        // Play Again?
+
+        // Else
+        // Show Life left
+        // Guess Again
+    }
+    
+    // If PlayAgain, reset HiddenWord and Life
+    // Else quit the game
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("subdermatoglyphic");
+    Lives = 4;
 }
